e4f54f7ed65dfb2ab5ac07de0df8572624d40992
tim-salabim <tim.appelhans@gmail.com>
2020-02-23 11:48:38 +0100

    scrapbooking

--- experiments/random_markers.R
+++ experiments/random_markers.R
@@ -5,7 +5,7 @@ library(sf)
 library(colourvalues)
 library(data.table)
 
-n = 2e5
+n = 1e3
 
 df1 = data.frame(id = 1:n,
                  id2 = n:1,
@@ -13,15 +13,22 @@ df1 = data.frame(id = 1:n,
                  y = rnorm(n, 49, 0.8))
 
 pts = st_as_sf(df1, coords = c("x", "y"), crs = 4326)
-pts$pop = leafpop::popupTable(pts)
+# pts$pop = leafpop::popupTable(pts)
 
 system.time({
   # cols = colour_values_rgb(pts$id, include_alpha = FALSE) / 255
+  # cols = colour_values(pts$id, include_alpha = FALSE)
 
   options(viewer = NULL)
 
   m = mapview()@map %>%
-    addGlPoints(data = pts, popup = "pop", group = "pts", digits = 5) %>%
+    addGlPoints(
+      data = pts
+      # , color = cols
+      , popup = NULL
+      , group = "pts"
+      , digits = 5
+    ) %>%
     addMouseCoordinates() %>%
     setView(lng = 10.5, lat = 49.5, zoom = 6) %>%
     mapview:::updateOverlayGroups(group = "pts")
@@ -32,49 +39,49 @@ m
 mapshot(m, "/home/timpanse/Desktop/test.html", selfcontained = FALSE)
 
 ### try 10 mio - partition into 4 chunks to avoid size overflow in the browser
-n = 1e7
-
-df1 = data.frame(id = 1:n,
-                 x = c(runif(n/4, -160, 0),
-                       runif(n/4, -160, 0),
-                       runif(n/4, 0, 160),
-                       runif(n/4, 0, 160)),
-                 y = c(runif(n/4, -80, 0),
-                       runif(n/4, 0, 80),
-                       runif(n/4, -80, 0),
-                       runif(n/4, 0, 80)))
-
-pts = st_as_sf(df1, coords = c("x", "y"), crs = 4326)
-
-cols = colour_values_rgb(1:4, include_alpha = FALSE) / 255
-
-system.time({
-  pts_lst = lapply(
-    split(as.data.table(pts), (as.numeric(rownames(pts))-1) %/% 2.5e6),
-    st_as_sf
-  )
-})
-#   user  system elapsed
-# 42.298   0.050  42.344
-
-m = mapview()@map %>%
-  addMouseCoordinates() %>%
-  setView(lng = 0, lat = 0, zoom = 2)
-
-
-for (i in 1:4) {
-  print(i)
-  m = leafgl:::addGlPointsSrc2(map = m,
-                                        data = pts_lst[[i]],
-                                        weight = i * 5,
-                                        color = cols[i, , drop = FALSE],
-                                        group = as.character(i),
-                                        popup = "id")
-}
-
-options(viewer = NULL)
-
-m %>%
-  mapview:::updateOverlayGroups(group = as.character(1:4))
-
-mapshot(m, "/home/timpanse/Desktop/test.html", selfcontained = FALSE)
+# n = 1e7
+#
+# df1 = data.frame(id = 1:n,
+#                  x = c(runif(n/4, -160, 0),
+#                        runif(n/4, -160, 0),
+#                        runif(n/4, 0, 160),
+#                        runif(n/4, 0, 160)),
+#                  y = c(runif(n/4, -80, 0),
+#                        runif(n/4, 0, 80),
+#                        runif(n/4, -80, 0),
+#                        runif(n/4, 0, 80)))
+#
+# pts = st_as_sf(df1, coords = c("x", "y"), crs = 4326)
+#
+# cols = colour_values_rgb(1:4, include_alpha = FALSE) / 255
+#
+# system.time({
+#   pts_lst = lapply(
+#     split(as.data.table(pts), (as.numeric(rownames(pts))-1) %/% 2.5e6),
+#     st_as_sf
+#   )
+# })
+# #   user  system elapsed
+# # 42.298   0.050  42.344
+#
+# m = mapview()@map %>%
+#   addMouseCoordinates() %>%
+#   setView(lng = 0, lat = 0, zoom = 2)
+#
+#
+# for (i in 1:4) {
+#   print(i)
+#   m = leafgl:::addGlPointsSrc2(map = m,
+#                                         data = pts_lst[[i]],
+#                                         weight = i * 5,
+#                                         color = cols[i, , drop = FALSE],
+#                                         group = as.character(i),
+#                                         popup = "id")
+# }
+#
+# options(viewer = NULL)
+#
+# m %>%
+#   mapview:::updateOverlayGroups(group = as.character(1:4))
+#
+# mapshot(m, "/home/timpanse/Desktop/test.html", selfcontained = FALSE)
